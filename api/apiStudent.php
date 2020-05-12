2e1c2f742da92c7f6264f90370269fe5e396d54b
unknown <wjddudrbs10045401@gmail.com>
2020-05-13 09:48:58 +1200

    show single student

--- api/apiStudent.php
+++ api/apiStudent.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
-if (isset($_SESSION['admin_id]'])) {
+//session_start();
+//if (isset($_SESSION['admin_id]'])) {
     include_once "../class/Administrator.php";
     $admin = new Administrator();
     $student = $admin->showStudents($_GET["id"]);
     echo json_encode($student);
-}else{
-    echo json_encode("login please");
-}
+//}else{
+//    echo json_encode("login please");
+//}
